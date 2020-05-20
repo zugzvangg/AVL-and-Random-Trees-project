5e0ced3e2ba43ae1e6947c8365eb5b353171795e
mirpapur <mrpepperbest@yandex.ru>
2020-05-20 14:17:16 +0300

    some upgrades: taken to outsource and made some tests.

---
+++ cmake-build-debug/CMakeFiles/AVL_and_Red-Black.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mark/programming/AVL_and_Red-Black/main.cpp" "/home/mark/programming/AVL_and_Red-Black/cmake-build-debug/CMakeFiles/AVL_and_Red-Black.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
